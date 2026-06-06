61a3cf49778824ffc2d6b8e0ff9c4e6acb1c4a8d
kacn1z-del <kacn1z@gmail.com>
2026-06-05 22:00:13 -0600

    Crear langs.js

---
+++ src/i18n/langs.js
@@ -0,0 +1,9 @@
+export const LANGS = [
+  { code: "es", flag: "🇨🇷", name: "Español" },
+  { code: "en", flag: "🇺🇸", name: "English" },
+  { code: "bribri", flag: "🌿", name: "Bribri" },
+  { code: "cabecar", flag: "🌿", name: "Cabécar" },
+  { code: "maleku", flag: "🌿", name: "Maleku" },
+  { code: "brunca", flag: "🌿", name: "Brunca" },
+  { code: "ngabe", flag: "🌿", name: "Ngäbe" },
+];
