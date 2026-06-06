31dbf06cdc54df5503f54296b8e672ee23da6e12
kacn1z-del <kacn1z@gmail.com>
2026-06-06 08:03:53 -0600

    Actualizar vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Voltara/', // Importante: nombre del repo
+  base: '/Voltara/', // ESTA LÍNEA ES OBLIGATORIA
 })
